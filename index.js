1e58e23c1580127e8749c832e984a9a457de2b2d
vihangatheturtle <vihanga1432@gmail.com>
2021-05-23 00:18:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const SHA256 = require('crypto-js/sha256');
 const packageData = require('./package.json');
 
 const app = express();
@@ -7,17 +8,37 @@ const port = 31823;
 
 var nodeData = {};
 
-app.post('/node/register-new-node', function(req, res) {
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
+}
+
+function splHalf(str) {
+	let half = Math.floor(str.length / 2);
+	return [str.slice(0, half), str.slice(half, str.length)]
+}
+
+app.post('/nodes/register-new-node', function(req, res) {
 	var uuid = uuidv4();
-	req.headers["machinename"] = "csms" + req.headers["machinename"] + 
+	var mnspl = splHalf(req.headers["machinename"]);
+	req.headers["machinename"] = "csms" + makeid(8) + mnspl[0] + makeid(8) + mnspl[1] + makeid(8) + "node";
 	try {
 		nodeData[uuid] = {"machineName":req.headers["machinename"],"uuid":uuid};
-	}
-	catch {
+	} catch {
 		res.json({"error":true,"message":"Invalid machinename"});
 		return;
 	}
 	res.json({"error":false,"message":nodeData[uuid]});
+});
+
+app.get('/nodes/node-list', function(req, res) {
+	res.send(nodeData);
 });
 
 app.get('/', function(req, res) {
