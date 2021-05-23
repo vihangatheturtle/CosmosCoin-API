2f38acc74aceea9a9048659dc52672a04dd45593
vihangatheturtle <vihanga1432@gmail.com>
2021-05-23 20:08:46 +0100

    Added transaction waitlist and hash verification

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ class CryptoBlockchain{
 
 var mineDiff = 4;
 var nodeData = {};
+var txNTBC = {};
 let CosmosCoin = new CryptoBlockchain();
 let tempCoin = new CryptoBlockchain();
 tempCoin.blockchain[0] = CosmosCoin.blockchain[0];
@@ -112,26 +113,97 @@ app.post('/nodes/register-new-node', function(req, res) {
 
 var blckMine = false;
 
-app.post('/mine/publish/block', function(req, res) {
-	if (!blckMine) {
-		blckMine = true;
-		tempCoin.addNewBlockRaw(new CryptoBlock(1, req.body, req.body["precedingHash"], true));
-		if (!tempCoin.checkChainValidity()) {
-			tempCoin = new CryptoBlockchain();
-			for (i=0; i<CosmosCoin.blockchain.length; i++) {
-				tempCoin.blockchain[i] = CosmosCoin.blockchain[i];
+app.post('/tx/transactions', function(req, res) {
+	var machineUUID = req.headers["uuid"];
+	if (machineUUID) {
+		try {
+			var txID = uuidv4();
+			var from = req.headers["from"];
+			var to = req.headers["to"];
+			var price = req.headers["price"];
+			if (price == null) {
+				res.send("invalidBodyMissingPrice");
+				return;
 			}
-			res.send('invalidBlock');
-		} else {
-			CosmosCoin.addNewBlockRaw(new CryptoBlock(1, req.body, req.body["precedingHash"], true));
-			if (CosmosCoin.checkChainValidity()) {
-				console.log('block mined');
-				res.send('validBlock');
+			if (to == null) {
+				res.send("invalidBodyMissingTo");
+				return;
 			}
+			if (from == null) {
+				res.send("invalidBodyMissingFrom");
+				return;
+			}
+			var time = Date.now().toString();
+			var hash = SHA256(to + from + txID + price).toString();
+			txNTBC[txID] = {"txBody":{"txFrom":from,"txTo":to,"txPrice":price},"txID":txID,"txTicks":Date.now().toString(),"txHash":hash};
+			res.send(txNTBC[txID]);
+		} catch (ex) {
+			console.log(ex);
+			res.send('err');
 		}
-		blckMine = false;
 	} else {
-		res.send('blockInProcess');
+		res.send('noUUIDProvided');
+		return;
+	}
+});
+
+app.get('/tx/transactions', function(req, res) {
+	res.send(JSON.stringify(txNTBC));
+});
+
+app.post('/mine/publish/block', function(req, res) {
+	var machineUUID = req.headers["uuid"];
+	var txIDTransaction = null;
+	if (req.headers["txid"] == null) {
+		res.send('noTxIDProvided');
+		return;
+	} else {
+		try {
+			if (txNTBC[req.headers["txid"]] == null) {
+				res.send('invalidTxID');
+				return;
+			} else if (txNTBC[req.headers["txid"]]["txHash"] != req.headers["txhash"]) {
+				res.send('invalidTxHash');
+				return;
+			} else {
+				txIDTransaction = txNTBC[req.headers["txid"]]["txID"];
+			}
+		} catch (ex) {
+			console.log(ex);
+			res.send('invalidTxID');
+			return;
+		}
+	}
+	if (req.headers["uuid"] == null) {
+		res.send('noUUIDProvided');
+		return;
+	}
+	if (nodeData[machineUUID]) {
+		if (!blckMine) {
+			blckMine = true;
+			tempCoin.addNewBlockRaw(new CryptoBlock(tempCoin.blockchain.length, req.body, req.body["precedingHash"], true));
+			if (!tempCoin.checkChainValidity()) {
+				tempCoin = new CryptoBlockchain();
+				for (i=0; i<CosmosCoin.blockchain.length; i++) {
+					tempCoin.blockchain[i] = CosmosCoin.blockchain[i];
+				}
+				res.send('invalidBlock');
+			} else {
+				CosmosCoin.addNewBlockRaw(new CryptoBlock(CosmosCoin.blockchain.length, req.body, req.body["precedingHash"], true));
+				delete txNTBC[req.headers["txid"]];
+				if (CosmosCoin.checkChainValidity()) {
+					console.log("[blockchain] +block:" + txIDTransaction + ":" + nodeData[machineUUID]["machineName"]);
+					res.send('validBlock');
+				}
+			}
+			blckMine = false;
+		} else {
+			res.send('blockInProcess');
+		}
+		return;
+	} else {
+		res.send('invalidUUID');
+		return;
 	}
 });
 
