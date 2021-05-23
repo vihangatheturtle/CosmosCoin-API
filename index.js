5a45caf20000dc3fcbd06bfa76e438b9622b60ad
vihangatheturtle <vihanga1432@gmail.com>
2021-05-23 16:22:33 +0100

    changed startup msg

--- index.js
+++ index.js
@@ -167,5 +167,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`CosmosCoin API server listening at http://localhost:${port}`)
 })
